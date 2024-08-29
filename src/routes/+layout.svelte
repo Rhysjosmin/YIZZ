12ac3e45520c74711a48a19e70983efb46a4550b
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2024-08-30 01:50:47 +0530

    responsiveness

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,8 @@
 	import Nav from '../components/nav.svelte';
 </script>
 
-<body class="bg-stone-950 min-h-screen text-white font-rubik ">
-	<main class=" relative  ">
+<body class="bg-stone-950  text-white font-rubik ">
+	<main class="   ">
     <Nav/>
 	<slot />
 	</main>
